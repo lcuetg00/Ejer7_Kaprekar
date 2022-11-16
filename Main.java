332b6bc4fef3165cfd3eb215e86c3cd6d692605e
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-11-16 23:15:54 +0100

    Funcionalidad terminada
    -Arreglado la comprobación de números Kaprekar. Ahora es capaz de identificar números como el 4879.
    -Añadido clase Consola, para pedir al usuario un número a comprobar si es Kaprekar.

--- Main.java
+++ Main.java
@@ -2,15 +2,8 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        //Comprobación Kaprekar del 1 al 10000
-        for(int i=4;i<10000;i++) {
-            BigInteger num = new BigInteger(Integer.toString(i));
-            if(NumeroKaprekar.comprobarKaprekar(num)) {
-                System.out.println("Num es kaprekar: "+i);
-            }
-        }
+        Consola consola = new Consola();
+        consola.iniciarConsola();
 
 
     }
--- NumeroKaprekar.java
+++ NumeroKaprekar.java
@@ -1,10 +1,32 @@
 import java.math.BigInteger;
 
+/**
+ * Clase con métodos estáticos para operaciones numéricas
+ */
 public class NumeroKaprekar {
 
+    /**
+     * Constructor privado para impedir que se cree una instancia de la clase (todos sus métodos son estáticos)
+     */
+    private NumeroKaprekar() {
+    }
     //Numeros kaprekar: enteros positivos no negativos
     //Su cuadrado se separa en dos sumandos que dan el dígito original
-    public static boolean comprobarKaprekar(final BigInteger numero) {
+
+    /**
+     * Comprueba si un número cumple las condiciones de ser Kaprekar
+     * Condiciones para que sea Kaprekar:
+     * -Debe de ser un entero positivo
+     * -Si se divide el cuadrado del número en dos mitades, la suma de esas dos mitades debe de ser el número original
+     * @param numero
+     * @return true si el número es Kaprekar
+     *         false si el número no es Kaprekar
+     * @throws IllegalArgumentException si numero es null
+     */
+    public static boolean comprobarKaprekar(final BigInteger numero) throws  IllegalArgumentException {
+        if(numero == null) {
+            throw new IllegalArgumentException("Clase NumeroKaprekar: comprobarKaprekar el BigInteger introducido es null");
+        }
         if(numero.compareTo(BigInteger.ZERO) == -1) {
             return false;
         }
@@ -16,17 +38,25 @@ public class NumeroKaprekar {
         BigInteger mitad1 = new BigInteger(cifraCuadrado.substring(mitad , cifraCuadrado.length()));
         BigInteger mitad2 = new BigInteger(cifraCuadrado.substring(0, mitad));
 
-        //Falta comprobación para el 4879 y 5292
-        //
-        //while(mitad2.mod(BigInteger.TEN) == BigInteger.ZERO) {
-        //    mitad2 = mitad2.divide(BigInteger.TEN);
-        //}
-
-
+        //Si la suma de las dos mitades da lugar al mismo número introducido, es un número Kaprekar
         if(mitad1.add(mitad2).compareTo(numero) == 0) {
             return true;
         } else {
-            return false;
+            //Casos especiales para los nñumeros Kaprekar
+            //Los números como  el 4879 o el 5292 la forma de separar en dos números es diferente.
+            //Ejemplo:
+            //4879^2 = 23804641
+            //2380 + 4641 = 7021 != 4879
+
+            //Para estos números, si a la izquierda de su mitad hay 0s, hay que añadirselos al otro sumando:
+            //238 + 04641 = 4879
+
+            //Aquí compruebo si la mitad de ese número contiene 0s a la derecha, si es así los quito
+            while(mitad2.mod(BigInteger.TEN) == BigInteger.ZERO) {
+                mitad2 = mitad2.divide(BigInteger.TEN);
+            }
+            //vuelvo a realizar la comprobación de su suma
+            return (mitad1.add(mitad2).compareTo(numero) == 0);
         }
     }
 }
