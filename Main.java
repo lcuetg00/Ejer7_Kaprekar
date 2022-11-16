cc7686698725cef08182a493d41e9b0a83cd706d
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-11-16 03:49:36 +0100

    Actualizado

--- Main.java
+++ Main.java
@@ -4,7 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        BigInteger num = new BigInteger("9");
-        NumeroKaprekar.comprobarKaprekar(num);
+        //Comprobación Kaprekar del 1 al 10000
+        for(int i=4;i<10000;i++) {
+            BigInteger num = new BigInteger(Integer.toString(i));
+            if(NumeroKaprekar.comprobarKaprekar(num)) {
+                System.out.println("Num es kaprekar: "+i);
+            }
+        }
+
+
     }
 }
--- NumeroKaprekar.java
+++ NumeroKaprekar.java
@@ -5,20 +5,23 @@ public class NumeroKaprekar {
     //Numeros kaprekar: enteros positivos no negativos
     //Su cuadrado se separa en dos sumandos que dan el dígito original
     public static boolean comprobarKaprekar(final BigInteger numero) {
-        //Si numero < 0
         if(numero.compareTo(BigInteger.ZERO) == -1) {
             return false;
         }
-        //
+        //Saco el número al cuadrado en un String
         String cifraCuadrado = numero.pow(2).toString();
         int mitad = (cifraCuadrado.length()/2);
-        if((cifraCuadrado.length() & 1) == 1) {
-            //si es impar
-            mitad=mitad+1;
-        }
 
-        BigInteger mitad1 = new BigInteger(cifraCuadrado.substring(0,mitad));
-        BigInteger mitad2 = new BigInteger(cifraCuadrado.substring(mitad, cifraCuadrado.length()));
+        //Separo el cuadrado del número en 2 números diferentes, construyendolos con la mitad del String cifraCuadrado
+        BigInteger mitad1 = new BigInteger(cifraCuadrado.substring(mitad , cifraCuadrado.length()));
+        BigInteger mitad2 = new BigInteger(cifraCuadrado.substring(0, mitad));
+
+        //Falta comprobación para el 4879 y 5292
+        //
+        //while(mitad2.mod(BigInteger.TEN) == BigInteger.ZERO) {
+        //    mitad2 = mitad2.divide(BigInteger.TEN);
+        //}
+
 
         if(mitad1.add(mitad2).compareTo(numero) == 0) {
             return true;
